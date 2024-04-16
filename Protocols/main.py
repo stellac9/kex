b1b975712f72e87bd6642b1eb042f8ecd3b5ead1
= <=>
2024-04-16 14:59:59 +0200

    Added comments

--- Protocols/main.py
+++ Protocols/main.py
@@ -3,20 +3,29 @@ from e91 import E91Scheme
 from b92 import B92Scheme
 from core import QKDResults
 
+"""
+Used to execute everything and print out result
+"""
 if __name__ == "__main__":
+    # three instances with no eavesdropper
     bb84 = BB84Scheme(False)
     e91 = E91Scheme(False)
     b92 = B92Scheme(False)
-    bb84_res = bb84.run(1000000)
+    # run() returns of type QKDResults, with input number of "shots"
+    bb84_res = bb84.run(1000000) 
     e91_res = e91.run(1000000)
     b92_res = b92.run(1000000)
 
+    # three instances with an eavesdropper
     bb84_e = BB84Scheme(True)
     e91_e = E91Scheme(True)
     b92_e = B92Scheme(True)
     bb84_e_res = bb84_e.run(1000000)
     e91_e_res = e91_e.run(1000000)
     b92_e_res = b92_e.run(1000000)
+    
+    # prints result of running QKD schemes (with and without eavesdropper)
+    # in terms of raw key efficiency and qubit error rate
     print("\n")
     print("BB84 RKE:\t", bb84_res.rke(), "\n")
     print("BB84 (w/ eve) RKE:\t", bb84_e_res.rke(), "\n")
