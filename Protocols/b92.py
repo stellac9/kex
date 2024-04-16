c1ec0890b1449046bbb79c8300ac4a9cdae557e1
= <=>
2024-04-16 15:30:56 +0200

    Added comments

--- Protocols/b92.py
+++ Protocols/b92.py
@@ -3,12 +3,15 @@ from qiskit_aer import QasmSimulator
 
 import core
 
-
+"""
+Defines B92 Scheme
+"""
 class B92Scheme(core.QKDScheme):
     def __init__(self, eavesdropper: bool):
         simulator = QasmSimulator()
-        qreg_q = QuantumRegister(1, 'q')
         
+        # initialises all registers that will be used
+        qreg_q = QuantumRegister(1, 'q')
         if eavesdropper:
             creg_e_bit = ClassicalRegister(1, 'e_bit')
             creg_e_certain = ClassicalRegister(1, 'e_certain')
@@ -16,32 +19,35 @@ class B92Scheme(core.QKDScheme):
         creg_r_bit = ClassicalRegister(1, 'r_bit')
         creg_r_certain = ClassicalRegister(1, 'r_certain')
         
+        # initialises circuit (with the registers that will be used) 
         if eavesdropper:
             circuit = QuantumCircuit(qreg_q, creg_e_bit, creg_e_certain, creg_s_bit, creg_r_bit, creg_r_certain)
         else:
             circuit = QuantumCircuit(qreg_q, creg_s_bit, creg_r_bit, creg_r_certain)
+            
+        # now gates are added:
 
-        # RNG for basis
+        # RNG for basis, randomises basis/bit that will be sent by Alice and used to measure by Bob
         circuit.h(qreg_q[0])
         circuit.measure(qreg_q[0], creg_r_bit[0])
         circuit.h(qreg_q[0])
         circuit.measure(qreg_q[0], creg_s_bit[0])
         circuit.barrier(qreg_q[0])
 
-        # Sender
+        # Sender, sends in Alice basis
         circuit.reset(qreg_q[0])
         circuit.h(qreg_q[0]).c_if(creg_s_bit, 1)
         circuit.barrier(qreg_q[0])
 
-        # Eavesdropper
+        # Eavesdropper, measures in basis and send measured bit in same basis
         if eavesdropper:
-            circuit.h(qreg_q[0]).c_if(creg_e_bit, 0)
+            circuit.h(qreg_q[0]).c_if(creg_e_bit, 0) # basis for Eve was never defined
             circuit.measure(qreg_q[0], creg_e_certain[0])
             circuit.reset(qreg_q[0])
             circuit.h(qreg_q[0]).c_if(creg_e_bit, 1)
 
 
-        # Receiver
+        # Receiver, measure in Bob's basis
         circuit.h(qreg_q[0]).c_if(creg_r_bit, 0)
         circuit.measure(qreg_q[0], creg_r_certain[0])
 
--- Protocols/e91.py
+++ Protocols/e91.py
@@ -3,32 +3,36 @@ from qiskit_aer import QasmSimulator
 
 import core
 
-
+"""
+Defines E91 Scheme
+"""
 class E91Scheme(core.QKDScheme):
     def __init__(self, eavesdropper: bool):
         simulator = QasmSimulator()
         
+        # initialises all registers that will be used and circuit
         qreg_q = QuantumRegister(2, 'q')
         creg_r_bit = ClassicalRegister(1, 'r_bit')
         creg_s_bit = ClassicalRegister(1, 's_bit')
         creg_r_bas = ClassicalRegister(1, 'r_bas')
         creg_s_bas = ClassicalRegister(1, 's_bas')
-        
         if eavesdropper:
             creg_e_bas = ClassicalRegister(1, 'e_bas')
             creg_e_bit = ClassicalRegister(1, 'e_bit')
             circuit = QuantumCircuit(qreg_q, creg_e_bit, creg_e_bas, creg_r_bit, creg_s_bit, creg_r_bas, creg_s_bas)
         else:
             circuit = QuantumCircuit(qreg_q, creg_r_bit, creg_s_bit, creg_r_bas, creg_s_bas)
+            
+        # now gates are added
 
-        # RNG for basis
+        # RNG for basis, randomises basis of Alice and Bob
         circuit.h(qreg_q[0])
         circuit.h(qreg_q[1])
         circuit.measure(qreg_q[0], creg_s_bas[0])
         circuit.measure(qreg_q[1], creg_r_bas[0])
         circuit.barrier(qreg_q[0], qreg_q[1])
 
-        # Entanglement
+        # Entanglement, by "bell gate"
         circuit.reset(qreg_q[0])
         circuit.reset(qreg_q[1])
         circuit.h(qreg_q[0])
@@ -37,12 +41,12 @@ class E91Scheme(core.QKDScheme):
 
         # Eavesdropper
         if eavesdropper:
-            circuit.h(qreg_q[1]).c_if(creg_e_bas, 1)
+            circuit.h(qreg_q[1]).c_if(creg_e_bas, 1) # Eve's basis was never defined
             circuit.measure(qreg_q[1], creg_e_bit[0])
             circuit.h(qreg_q[1]).c_if(creg_e_bas, 1)
             circuit.barrier(qreg_q[0], qreg_q[1])
 
-        # Measurement
+        # Measurement of Alice and Bob
         circuit.h(qreg_q[0]).c_if(creg_s_bas, 1)
         circuit.measure(qreg_q[0], creg_s_bit[0])
         circuit.h(qreg_q[1]).c_if(creg_r_bas, 1)
